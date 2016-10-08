b3dad7b2e866d6d07a97f9f7a651c4118fc46833
aM3z <amezolma@plu.edu>
2016-10-08 10:48:05 -0700

    start index page

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<!--
+
+================================================================================
+================================================================================
+================================================================================
+ 
+TEAM:		Micaela Pierce and Miguel Amezola
+POJECT: 	1
+COURSE: 	CSCI387
+DATE: 		2016-10-18
+
+================================================================================
+============================================ https://www.cs.plu.edu ============
+================================================================================
+
+-->
+
+<html lang="en">
+
+	<head>
+		<!-- meta tags -->
+		<meta charset="utf-8">
+		<title>Rescue Dogs and Cats</title>
+		<meta name="description" content="This is a multi-page website for rescuing two types of pets or animals.">
+		<meta name="robots" content="index, follow">
+		<meta name="author" content="Micaela Pierce, Miguel Amezola">
+
+		<!-- styling -->
+		<link rel="stylesheet" href="" />
+		<link rel="stylesheet" type="text/css" href="">
+
+	</head>
+	<body> 
+		<div class="container">
+
+			<header>
+				<div class="main-wrapper">	
+					<h1><a id="homepage-link" href="">Rescue Dogs and Cats</a></h1>
+				</div>	
+			</header> 
+
+			<nav>
+				<div class="main-wrapper">
+					<!-- first dropdown -->
+					<div class="dropdown">
+						<a href="" class="menubtn">Dogs</a>
+						<!--
+						<div class="dropdown-content">
+							<a href=""></a>
+						</div>
+						-->
+					</div>
+					<!-- second dropdown -->
+					<div class="dropdown">
+						<a href="" class="menubtn">Cats</a>
+						<!--
+						<div class="dropdown-content">
+							<a href=""></a>
+						</div>
+						-->
+					</div>
+					<!-- first dropdown -->
+					<div class="dropdown">
+						<a href="" class="menubtn">About Us</a>
+						<!--
+						<div class="dropdown-content">
+							<a href=""></a>
+						</div>
+						-->
+					</div>
+				</div>
+			</nav>
+			
+			<div class="main-wrapper" id="social-media-buttons">
+			<a href="https://twitter.com/share" class="twitter-share-button" data-text="This website is a homework project for CSCI387 course @PLUNEWS and is not a real travel site." data-url="https://www.cs.plu.edu/~amezolma/homework/hw3" data-via="aM3zola" data-show-count="false">Tweet</a>
+			</div>
+
+
+			<main role="main" class="clear main-wrapper">
+						
+				<div class="sidebar1">
+				</div>
+
+			
+				<article> 
+					<header>
+						<h1>Come Catch the Big One!</h1>
+						<p>Published: <time pubdate="pubdate">2016-10-07</time> </p>
+						<hr>
+					</header>
+				</article> 
+
+			</main>
+
+			<footer class="skinny"> 
+				<p class="warning-msg">This website is a homework project by Miguel Amezola for PLU's CSCI387 course and is not a real travel site.</p>
+				<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+			</footer>
+
+		</div> <!-- end div.container -->
+
+		<!-- scripts -->
+		<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		
+	</body> 
+
+</html>
+
