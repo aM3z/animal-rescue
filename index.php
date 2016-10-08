776165fe554f81d1c5c335124f483856ba411ec3
aM3z <amezolma@plu.edu>
2016-10-08 10:49:36 -0700

    change skinny to main-wrapper

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ DATE: 		2016-10-18
 
 			</main>
 
-			<footer class="skinny"> 
+			<footer class="main-wrapper"> 
 				<p class="warning-msg">This website is a homework project by Miguel Amezola for PLU's CSCI387 course and is not a real travel site.</p>
 				<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 			</footer>
