f156f0285a6fa159463f090da482507430a82377
micpierce <piercemm@plu.edu>
2016-10-15 06:17:34 -0700

    homepage layout
    html and bare css for homepage
    
    slight header mods

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php 
+
+	require('./templates/header.php'); 
+
+?>
+
+			<main role="main" class="clear main-wrapper">
+				
+				<?php require('./templates/sidebar1.php'); ?>			
+				<div class="sidebar1">
+				</div>
+
+				<section class="home">
+					<article> 
+						<header>
+							<h2>Welcome!</h2>
+						</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia nec ipsum id interdum. Nunc porta, lorem vitae aliquam gravida, sem nunc vestibulum lorem, sed tempus sem tellus eu turpis. Nullam et enim vitae eros pretium finibus. Praesent lorem elit, vestibulum sit amet hendrerit a, vulputate ac nulla. Ut quis enim eget tellus aliquet fermentum. Proin tristique sagittis augue ut congue. Nam in nisl a tellus fermentum aliquet in ut elit. Quisque semper suscipit nunc, vel euismod odio ullamcorper ut.
+							<br><br>
+							Phasellus quis tellus at lectus imperdiet pellentesque eget quis libero. Fusce placerat pellentesque massa vitae fermentum. Cras ullamcorper vestibulum lacus vel posuere. Proin ornare vitae lacus ac dictum. Nullam malesuada dictum diam ut eleifend. Maecenas dapibus rhoncus lectus sed scelerisque. Fusce aliquam magna ac lacus placerat volutpat. Integer bibendum efficitur tellus, at ullamcorper magna dignissim ac. Mauris a ultricies lectus, ac pulvinar purus. Suspendisse aliquam augue est, sed hendrerit metus facilisis eu. Aliquam in diam dui. Sed at tincidunt nisi, a ornare metus. Suspendisse dictum leo eget lacus suscipit, id ornare nunc fringilla. Integer accumsan elementum mauris, ac aliquam urna vestibulum ac. Vivamus a ipsum eu tellus vestibulum ornare quis nec elit.
+						</p>
+					</article> 
+					<article> 
+						<header>
+							<h2>Title Two!</h2>
+						</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia nec ipsum id interdum. Nunc porta, lorem vitae aliquam gravida, sem nunc vestibulum lorem, sed tempus sem tellus eu turpis. Nullam et enim vitae eros pretium finibus. Praesent lorem elit, vestibulum sit amet hendrerit a, vulputate ac nulla. Ut quis enim eget tellus aliquet fermentum. Proin tristique sagittis augue ut congue. Nam in nisl a tellus fermentum aliquet in ut elit. Quisque semper suscipit nunc, vel euismod odio ullamcorper ut.
+							<br><br>
+							Phasellus quis tellus at lectus imperdiet pellentesque eget quis libero. Fusce placerat pellentesque massa vitae fermentum. Cras ullamcorper vestibulum lacus vel posuere. Proin ornare vitae lacus ac dictum. Nullam malesuada dictum diam ut eleifend. Maecenas dapibus rhoncus lectus sed scelerisque. Fusce aliquam magna ac lacus placerat volutpat. Integer bibendum efficitur tellus, at ullamcorper magna dignissim ac. Mauris a ultricies lectus, ac pulvinar purus. Suspendisse aliquam augue est, sed hendrerit metus facilisis eu. Aliquam in diam dui. Sed at tincidunt nisi, a ornare metus. Suspendisse dictum leo eget lacus suscipit, id ornare nunc fringilla. Integer accumsan elementum mauris, ac aliquam urna vestibulum ac. Vivamus a ipsum eu tellus vestibulum ornare quis nec elit.
+						</p>
+					</article> 
+				</section>
+				<aside class="home">
+					<?php require('./templates/sidebar1.php'); ?>
+				</aside>
+			</main>
+			
+			<?php require('./templates/footer.php'); ?>
