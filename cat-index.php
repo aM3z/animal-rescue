a5c3a5bd61c6a1c5b844808066bda839d0c3ff57
micpierce <piercemm@plu.edu>
2016-10-15 06:46:13 -0700

    pages
    cat index and credits page made

---
+++ cat-index.php
@@ -0,0 +1,15 @@
+<?php 
+
+	require('./templates/header.php'); 
+
+?>
+
+	<main role="main" class="clear main-wrapper">
+		<h2>Avaliable Cats</h2>
+	</main>
+			
+<?php 
+
+	require('./templates/footer.php'); 
+
+?>
