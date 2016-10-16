bfb0af701b08e83cb26dd632f903875080befe05
amezolma <amezolma@haven.cs.plu.edu>
2016-10-16 01:00:15 -0700

    add image citations

--- credits.php
+++ credits.php
@@ -10,11 +10,88 @@
 					<header>
 						<h1>Citations</h1>
 					</header>
-						<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tortor odio, hendrerit at aliquam efficitur, convallis in purus. Donec sit amet libero elit. Etiam tristique, felis vel convallis pellentesque, arcu ante ornare erat, at vestibulum felis ex vel elit. Proin at sapien lectus. Nullam molestie dui vel malesuada posuere. Mauris pharetra accumsan dapibus. Nulla vel hendrerit est, quis malesuada lacus. Praesent feugiat bibendum ipsum eu egestas.
-						</p><p>
-						Sed imperdiet mauris a massa bibendum, sit amet placerat erat luctus. Quisque malesuada aliquet purus, a iaculis ligula suscipit sed. Curabitur pretium, metus a vehicula finibus, justo mi accumsan neque, a gravida mi urna quis enim. Nullam in iaculis neque. Ut venenatis nec est non tempus. Donec ultrices suscipit ultrices. Cras tincidunt sapien nunc, eu pulvinar odio scelerisque at.
-						</p>	
+					<h2>Dog Images</h2>
+					<ol>
+						<li>Welsh_Springer_Spaniel.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AWelsh_Springer_Spaniel.jpg
+						By Lokal_Profil (Own work) [GFDL (http://www.gnu.org/copyleft/fdl.html), CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0/) or CC BY 2.5 (http://creativecommons.org/licenses/by/2.5)], via Wikimedia Commons
+
+						<li>Rottweiler_standing_facing_left.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ARottweiler_standing_facing_left.jpg
+						By Dr. Manfred Herrmann Allgemeiner Deutscher Rottweiler-Klub (ADRK) e.V. http://www.ADRK.de (Image:Rottweiler.jpg) [GFDL (http://www.gnu.org/copyleft/fdl.html) or CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0/)], via Wikimedia Commons
+
+						<li>Vorderkoerpertiefstellung_THWZ.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AVorderkoerpertiefstellung_THWZ.jpg
+						By Thomas Zimmermann (THWZ) (Own work) [CC BY-SA 3.0 de (http://creativecommons.org/licenses/by-sa/3.0/de/deed.en)], via Wikimedia Commons
+
+						<li>Jumping_dog_Ryder.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AJumping_dog_Ryder.jpg
+						By TheGiantVermin, Guelph, Ontario, Canada (http://www.flickr.com/photos/tudor/2443796981/) [CC BY-SA 2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons
+
+						<li>American_Foxhound_and_Labrador_Retriever_playing.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AAmerican_Foxhound_and_Labrador_Retriever_playing.jpg
+						By Peter Wadsworth (Flickr) [CC BY 2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons
+
+						<li>Nova_Scotia_duck_tolling_retriever.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ANova_Scotia_duck_tolling_retriever.jpg
+						By Richardpaas (Own work) [Public domain], via Wikimedia Commons
+
+						<li>Sleeping_Pups.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ASleeping_Pups.jpg
+						By Bev Sykes from Davis, CA, USA (Flickr) [CC BY 2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons
+
+						<li>Langhaardackel_merlin_2005.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ALanghaardackel_merlin_2005.jpg
+						By Rainer Spickmann (Own work) [CC BY-SA 2.5 (http://creativecommons.org/licenses/by-sa/2.5)], via Wikimedia Commons
+
+						<li>Little_tootie.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ALittle_tootie.JPG
+						By Portraitlady4306 (Own work) [Public domain], via Wikimedia Commons
+
+					</ol>
+
+					<h2>Cat Images</h2>
+					<ol>
+						<li>Gustav_chocolate.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AGustav_chocolate.jpg
+						By Karin Langner-Bahmann (Own work) [GFDL (http://www.gnu.org/copyleft/fdl.html), CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0/) or CC BY-SA 2.5-2.0-1.0 (http://creativecommons.org/licenses/by-sa/2.5-2.0-1.0)], via Wikimedia Commons
+
+						<li>american-curl.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AGIC_Minnikatin_Zelta_ACS_n_22_female_EX1_CAGCIB_GIC.JPG
+						By Heikki Siltala ([1]) [CC BY 3.0 (http://creativecommons.org/licenses/by/3.0)], via Wikimedia Commons
+
+						<li>Birmakatze_Seal-Point.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ABirmakatze_Seal-Point.jpg
+						By grapheum.de (Own work) [CC BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons
+
+						<li>British_Longhair.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ABritish_Longhair_-_Black_Silver_Shaded.jpg
+						By Chatterie des Millenovæ http://www.millenovae.com (Own work) [GFDL (http://www.gnu.org/copyleft/fdl.html) or CC BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons
+
+						<li>British_Shorthair.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ABritish_Shorthair%2C_Classic_Tabby.jpg
+						Kattefreakske [GFDL (http://www.gnu.org/copyleft/fdl.html), CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0/) or CC BY-SA 2.5-2.0-1.0 (http://creativecommons.org/licenses/by-sa/2.5-2.0-1.0)], via Wikimedia Commons
+
+						<li>Egyptian_Mau_Bronze.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AEgyptian_Mau_Bronze.jpg
+						By liz west from Boxborough, MA (Egyptian Mau) [CC BY 2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons
+
+						<li>korat.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AVeda%2Cchat-adulte-m%C3%A2le-race-korat.JPG
+						By Veda Napha Naramit (Own work) [Public domain], via Wikimedia Commons
+
+						<li>BillyHJay.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ABillyHJay.jpg
+						By No machine-readable author provided. Whjayg assumed (based on copyright claims). [Public domain], via Wikimedia Commons
+
+						<li>Noorse-boskat.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3ANoorse-boskat_wikipedia_1.JPG
+						Wieke de Rijk, Netherlands at nl.wikipedia [CC BY 2.5 (http://creativecommons.org/licenses/by/2.5)], via Wikimedia Commons
+
+						<li>Scottish_fold.jpg</li>
+						https://commons.wikimedia.org/wiki/File%3AScottish_fold.jpg
+						By No machine-readable author provided. Portlembec~commonswiki assumed (based on copyright claims). [Public domain], via Wikimedia Commons
+						</ol>
 				</article>			
 			</main>
 <?php 
@@ -22,3 +99,4 @@
 	require('./templates/footer.php'); 
 
 ?>
+
