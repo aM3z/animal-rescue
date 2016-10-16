4ad9c20a3182234d31c002916adc7c49496cba5e
micpierce <piercemm@plu.edu>
2016-10-15 20:23:30 -0700

    index, detail page design first pass

--- details/dogs/yorkshire-terrier.php
+++ details/dogs/yorkshire-terrier.php
@@ -1,25 +1,33 @@
 <?php 
 
 	require('../../config.php');
-	require('../../templates/header.php'); 
+	require('./../../templates/header.php'); 
+	
 
 ?>
-
-			<main role="main" class="clear main-wrapper">
+<main role="main" class="clear main-wrapper">
 				
-				<article> 
-					<header>
-						<h1>Yorkshir Terrier</h1>
-						<p>Published: <time pubdate="pubdate">2016-10-07</time> </p>
-						<hr>
-					</header>
-
-                    <img  class="pet-pix" src="<?php print IMAGES; ?>/dogs/Little_tootie.jpg" alt="Yorkshire Terrier">
-
-				</article>
-
-				<?php require('../../templates/sidebar1.php'); ?>
-
-			</main>
+	<section class="details">
+		<article> 
+			<header>
+				<h1>Meet Frodo!</h1>
+			</header>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia nec ipsum id interdum. Nunc porta, lorem vitae aliquam gravida, sem nunc vestibulum lorem, sed tempus sem tellus eu turpis. Nullam et enim vitae eros pretium finibus. Praesent lorem elit, vestibulum sit amet hendrerit a, vulputate ac nulla. Ut quis enim eget tellus aliquet fermentum. Proin tristique sagittis augue ut congue. Nam in nisl a tellus fermentum aliquet in ut elit. Quisque semper suscipit nunc, vel euismod odio ullamcorper ut.
+				<br><br>
+				Phasellus quis tellus at lectus imperdiet pellentesque eget quis libero. Fusce placerat pellentesque massa vitae fermentum. Cras ullamcorper vestibulum lacus vel posuere. Proin ornare vitae lacus ac dictum. Nullam malesuada dictum diam ut eleifend. Maecenas dapibus rhoncus lectus sed scelerisque. Fusce aliquam magna ac lacus placerat volutpat. Integer bibendum efficitur tellus, at ullamcorper magna dignissim ac. Mauris a ultricies lectus, ac pulvinar purus. Suspendisse aliquam augue est, sed hendrerit metus facilisis eu. Aliquam in diam dui. Sed at tincidunt nisi, a ornare metus. Suspendisse dictum leo eget lacus suscipit, id ornare nunc fringilla. Integer accumsan elementum mauris, ac aliquam urna vestibulum ac. Vivamus a ipsum eu tellus vestibulum ornare quis nec elit.
+			</p>
+		</article> 
+	</section>
+	<aside class="details">
+		<h2>Facts About Frodo:</h2>
+		<ul>
+			<li><b>AGE:</b> adult</li>
+			<li><b>SEX:</b> male</li>
+			<li><b>BREED:</b> yorkshire terrior</li>
+			<li><b>SIZE:</b> toy</li>
+		</ul>
+	</aside>
+</main>
 
 <?php require('../../templates/footer.php'); ?>
