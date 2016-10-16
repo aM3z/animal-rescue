f78a36f7197081803693ec41a547f1233afa7e9f
micpierce <piercemm@plu.edu>
2016-10-16 12:51:01 -0700

    detail page and css

--- details/dogs/yorkshire-terrier.php
+++ details/dogs/yorkshire-terrier.php
@@ -9,24 +9,33 @@
 				
 	<section class="details">
 		<article> 
-			<header>
+			<header style="	background-image:url('./../../images/dogs/yorkie.jpg');
+ background-position-y:18%">
 				<h1>Meet Frodo!</h1>
 			</header>
+			<ul>
+				<li><div class=imghold><img src="./../../images/dogs/little_tootie.jpg"/></div></li>
+				<li><div class=imghold><img src="./../../images/dogs/yorkie.jpg"/></div></li>
+				<li><div class=imghold><img src="./../../images/dogs/yorkshire-terrier-06.jpg"/></div></li>
+			</ul>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia nec ipsum id interdum. Nunc porta, lorem vitae aliquam gravida, sem nunc vestibulum lorem, sed tempus sem tellus eu turpis. Nullam et enim vitae eros pretium finibus. Praesent lorem elit, vestibulum sit amet hendrerit a, vulputate ac nulla. Ut quis enim eget tellus aliquet fermentum. Proin tristique sagittis augue ut congue. Nam in nisl a tellus fermentum aliquet in ut elit. Quisque semper suscipit nunc, vel euismod odio ullamcorper ut.
+				Frodo is an adventurous little fellow. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia nec ipsum id interdum. Nunc porta, lorem vitae aliquam gravida, sem nunc vestibulum lorem, sed tempus sem tellus eu turpis. Nullam et enim vitae eros pretium finibus. Praesent lorem elit, vestibulum sit amet hendrerit a, vulputate ac nulla. Ut quis enim eget tellus aliquet fermentum. Proin tristique sagittis augue ut congue. Nam in nisl a tellus fermentum aliquet in ut elit. Quisque semper suscipit nunc, vel euismod odio ullamcorper ut.
 				<br><br>
-				Phasellus quis tellus at lectus imperdiet pellentesque eget quis libero. Fusce placerat pellentesque massa vitae fermentum. Cras ullamcorper vestibulum lacus vel posuere. Proin ornare vitae lacus ac dictum. Nullam malesuada dictum diam ut eleifend. Maecenas dapibus rhoncus lectus sed scelerisque. Fusce aliquam magna ac lacus placerat volutpat. Integer bibendum efficitur tellus, at ullamcorper magna dignissim ac. Mauris a ultricies lectus, ac pulvinar purus. Suspendisse aliquam augue est, sed hendrerit metus facilisis eu. Aliquam in diam dui. Sed at tincidunt nisi, a ornare metus. Suspendisse dictum leo eget lacus suscipit, id ornare nunc fringilla. Integer accumsan elementum mauris, ac aliquam urna vestibulum ac. Vivamus a ipsum eu tellus vestibulum ornare quis nec elit.
+				Phasellus quis tellus at lectus imperdiet pellentesque eget quis libero. Fusce placerat pellentesque massa vitae fermentum. Cras ullamcorper vestibulum lacus vel posuere. Proin ornare vitae lacus ac dictum. Nullam malesuada dictum diam ut eleifend. Maecenas dapibus rhoncus lectus sed scelerisque. Fusce aliquam magna ac lacus placerat volutpat. 
 			</p>
 		</article> 
 	</section>
 	<aside class="details">
-		<h2><b>Facts</b> About Frodo:</h2>
-		<ul>
-			<li><b>AGE</b> adult</li>
-			<li><b>SEX</b> male</li>
-			<li><b>BREED</b> yorkshire terrior</li>
-			<li><b>SIZE</b> toy</li>
-		</ul>
+		<article>
+			<h2><b>Facts</b> About Frodo:</h2>
+			<ul>
+				<li><b>AGE</b> adult</li>
+				<li><b>SEX</b> male</li>
+				<li><b>BREED</b> yorkshire terrior</li>
+				<li><b>SIZE</b> toy</li>
+			</ul>
+		</article>
+		<center><button class="adopt">ADOPT ME!</button></center>
 	</aside>
 </main>
 
