f0a38b84556528aeb6b5972a2ba579d0ae5aedbc
amezolma <amezolma@haven.cs.plu.edu>
2016-10-16 09:26:53 -0700

    add banner to homepage

--- index.php
+++ index.php
@@ -5,16 +5,24 @@
 	require('./templates/header.php'); 
 
 ?>
-
+		
+			<div class="banner">
+				<div class="call-to-action main-wrapper">
+					<p>Pet Lovers Unite</p>
+					<button class="default" type="button">Learn more</button>
+				</div>
+			</div>
 			<main role="main" class="clear main-wrapper">
-				
+
+							
 				<article> 
 					<header>
 						<h1>Welcome!</h1>
 					</header>
+
+					We rescue dogs and cats.
 				</article>
 
-				<?php require('./templates/sidebar1.php'); ?>
 
 			</main>
 
