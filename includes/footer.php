7b1bd91a6d351a8a4c3d0aa9c4c2e5d760e71ad3
micpierce <piercemm@plu.edu>
2016-10-17 22:27:53 -0700

    footer polish

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,10 @@
-			<footer class="main-wrapper"> 
-				<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+		<footer class="main-wrapper"> 
+				<p>
+					<b><u>PLU CSCI 387 PROJECT 1</u></b> was created by Miguel Amezola and Micaela Pierce fall semester, 10/18/2016. All information displayed by it is fictatious; it is not a real rescue site. 
+				</p>
+				<p>
+					<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+				</p>
 			</footer>
 
 		</div> <!-- end div.container -->
