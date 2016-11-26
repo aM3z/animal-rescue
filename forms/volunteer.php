2e834fbf1b273a99b3c6a223ba3f09e0192d0659
amezolma <amezolma@haven.cs.plu.edu>
2016-11-25 23:47:47 -0800

    style volenteer form

--- forms/volunteer.php
+++ forms/volunteer.php
@@ -15,29 +15,31 @@
 		<section class="form_fields">
 
 			<!-- current date -->
-			<label for="vol-current-date" >Date</label>
-			<input type="date" id="vol-current-date" name="volDate" value="<?php print date('Y-m-d'); ?>">
+			<fieldset id="vol-date">
+				<legend>Date</legend>
+				<input type="date" id="vol-current-date" name="volDate" value="<?php print date('Y-m-d'); ?>">
+			</fieldset>
 			<br>
 
 
 			<!-- contact information (name, email, address, phone) -->
-			<fieldset id="vol-contact-info">
+			<fieldset class="vol-contact" id="vol-contact-info">
 
 				<legend>Contact Information</legend>
 
-				<label for="vol-name">Name</label>
+				<label class="vol-label" for="vol-name">Name</label>
 				<input type="text" id="vol-name" name="volName" placeholder="Alan Turing" autofocus>
 				<br>
 
-				<label for="vol-email">Email</label>
+				<label class="vol-label" for="vol-email">Email</label>
 				<input type="email" id="vol-email" name="volEmail" placeholder="alan.turing@email.com">
 				<br>
 
-				<label for="vol-address">Address</label>
+				<label class="vol-label" for="vol-address">Address</label>
 				<input type="text" id="vol-address" name="volAddress">
 				<br>
 
-				<label for="vol-phone">Phone</label>
+				<label class="vol-label" for="vol-phone">Phone</label>
 				<input type="telephone" id="vol-phone" name="volPhone" placeholder="(253) 555-1234">
 
 			</fieldset>
@@ -48,11 +50,11 @@
 			<fieldset id="vol-personal-details">
 				<legend>Personal Details</legend>
 
-				<label for="vol-dob" >Date of Birth</label>
+				<label class="vol-label" for="vol-dob" >Date of Birth</label>
 				<input type="date" id="vol-dob" name="volDOB" value="<?php print date("Y-m-d", strtotime('-18 year') ); ?>">
 				<br>
 
-				<label for="vol-sex" >Sex</label>
+				<label class="vol-label" for="vol-sex" >Sex</label>
 				<select id="vol-sex" name="volSex">
 					<option></option>
 					<option value="male">Male</option>
@@ -64,22 +66,22 @@
 			<br>
 
 			<!-- emergency contact information (name, email, address, phone) and relationship to you -->
-			<fieldset id="vol-emergency-contant">
+			<fieldset class="vol-contact" id="vol-emergency-contant">
 				<legend>Emergency Contact</legend>
 
-				<label for="vol-emer-name">Name</label>
+				<label class="vol-label" for="vol-emer-name">Name</label>
 				<input type="text" id="vol-emer-name" name="volEmerName" placeholder="Alan Turing" autofocus>
 				<br>
 
-				<label for="vol-emer-email">Email</label>
+				<label class="vol-label" for="vol-emer-email">Email</label>
 				<input type="email" id="vol-emer-email" name="volEmerEmail" placeholder="alan.turing@email.com">
 				<br>
 
-				<label for="vol-emer-address">Address</label>
+				<label class="vol-label" for="vol-emer-address">Address</label>
 				<input type="text" id="vol-emer-address" name="volEmerAddress">
 				<br>
 
-				<label for="vol-emer-phone">Phone</label>
+				<label class="vol-label" for="vol-emer-phone">Phone</label>
 				<input type="telephone" id="vol-emer-phone" name="volEmerPhone" placeholder="(253) 555-1234">
 
 			</fieldset>
@@ -100,13 +102,13 @@
 
 				?>
 
-				<fieldset id="vol-avail-<?php echo $short_lower; ?>">
+				<fieldset class="vol-avail" id="vol-avail-<?php echo $short_lower; ?>">
 				<legend><?php echo $day; ?></legend>
 
-					<label for="vol-avail-<?php echo $short_lower; ?>-start">Start</label>
+					<label class="vol-avail-label" for="vol-avail-<?php echo $short_lower; ?>-start">Start</label>
 					<input type="time" id="vol-avail-<?php echo $short_lower; ?>-start" name="volAvail<?php echo $short; ?>Start" min="07:00:00" max="00:00:00" value="07:00:00">
 
-					<label for="vol-avail-<?php echo $short_lower; ?>-end">End</label>
+					<label class="vol-avail-label" for="vol-avail-<?php echo $short_lower; ?>-end">End</label>
 					<input type="time" id="vol-avail-<?php echo $short_lower; ?>-end" name="volAvail<?php echo $short; ?>End" min="07:00:00" max="00:00:00" value="00:00:00">
 
 				</fieldset>
