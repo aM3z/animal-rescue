495b255dbbc70036ee9747f946c1dd30f485fac1
amezolma <amezolma@haven.cs.plu.edu>
2016-11-27 09:22:28 -0800

    add cleanData.php, saveData.php, and data/ directory

---
+++ includes/cleanData.php
@@ -0,0 +1,24 @@
+<?php 
+
+	/**
+	 * Cleans and validates user-entered data.
+	 *
+	 * This functions strips off leading/ending whitespaces,
+	 * backslahes, and HTML/PHP tags from a given string. 
+	 * 
+	 * NOTE: strip_tags() is a built-in functions that has not
+	 * been discussed in class.
+	 *
+	 * @param string $data user-entered data
+	 * @return string
+	 */
+	function cleanData($data) {
+		// strip leading/ending whitespaces
+		$data = trim($data);
+		// strip slashes
+		$data = stripslashes($data);
+		// strip HTML and PHP tags
+		$data = strip_tags($data);
+		return $data;
+	}
+
---
+++ includes/saveData.php
@@ -0,0 +1,21 @@
+<?php
+
+	/**
+	 * Writes cleaned and validated data in JSON format to a .json file.
+	 *
+	 */
+	function saveData($data, $filename) {
+
+		// open file
+		$saveFile = fopen($filename, "a") or die("<p>Unable to open file...</p><br>");
+
+		// get JSON representation
+		$json_data = json_encode($data) . "\n";
+		
+		// write to file
+		fwrite($saveFile, $json_data);
+
+		// close file
+		fclose($orderFile);
+
+	}
