41bd9742ea8c798dfa31351a667985af353a9582
amezolma <amezolma@haven.cs.plu.edu>
2016-11-27 20:20:13 -0800

    more validation

--- includes/volunteerProcess.php
+++ includes/volunteerProcess.php
@@ -47,16 +47,33 @@
 		if ( $_SERVER['REQUEST_METHOD'] == 'POST' && $_POST ) {
 
 			// $volDate
+			$volDate = $_POST['volDate'];
+
 			// $volName
+			$volName = cleanData($_POST['volName']);
+
 			// $volEmail
+	
 			// $volAddress
+			$volAddress = cleanData($_POST['volAddress']);
+
 			// $volPhone
+
 			// $volDOB
+
 			// $volSex
-			// $volEmerName 
+			$volSex = $_POST['volSex'];
+
+			// $volEmerName
+			$volEmerName = cleanData($_POST['volEmerName']);
+
 			// $volEmerEmail
+
 			// $volEmerAddress
+			$volEmerAddress = cleanData($_POST['volEmerAddress']);
+
 			// $volEmerPhone
+
 			// $volAvailMonStart
 			// $volAvailMonEnd
 			// $volAvailTueStart
@@ -71,10 +88,19 @@
 			// $volAvailSatEnd
 			// $volAvailSunStart
 			// $volAvailSunEnd
+
 			// $volMaxHours
+			$volMaxHours = $_POST['volMaxHours'];
+
 			// $volMaxHoursPeriod
+			$volMaxHoursPeriod = $_POST['volMaxHoursPeriod'];
+
 			// $volExperience
+			$volExperience = cleanData($_POST['volExperience']);
+
 			// $volPref
+			$volPref = cleanData($_POST['volPref']);
+
 			// $volAgrees
 			if(isset($_POST['volAgrees'])) {
 				$volAgrees		= $_POST['volAgrees'];
@@ -150,37 +176,26 @@
 
 		switch ( $field ) {
 
-			// $volDate
-			// $volName
-			// $volEmail
-			// $volAddress
-			// $volPhone
-			// $volDOB
-			// $volSex
-			// $volEmerName 
-			// $volEmerEmail
-			// $volEmerAddress
-			// $volEmerPhone
-			// $volAvailMonStart
-			// $volAvailMonEnd
-			// $volAvailTueStart
-			// $volAvailTueEnd
-			// $volAvailWedStart
-			// $volAvailWedEnd
-			// $volAvailThuStart
-			// $volAvailThuEnd
-			// $volAvailFriStart
-			// $volAvailFriEnd
-			// $volAvailSatStart
-			// $volAvailSatEnd
-			// $volAvailSunStart
-			// $volAvailSunEnd
-			// $volMaxHours
-			// $volMaxHoursPeriod
-			// $volExperience
-			// $volPref
-			// $volAgrees
+			// date
+			case 'date' :  {
 
+			}
+			// email
+			case 'email' : {
+
+			}
+			// phone
+			case 'phone' : {
+
+			}
+			// $DOB
+			case 'dob' : {
+
+			}
+			// time_period
+			case 'time_period' : {
+
+			}
 
 			default :   break;
 		}
